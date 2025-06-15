55987e3d6ec7285f79bb47474a2ce76b24e6fdd4
nunezag816 <nunezag@msoe.edu>
2025-06-15 18:19:13 -0500

    Update Converter.java

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+    public static String toPoundsAndOunces(int ounces) {
+        int pounds = ounces / 16;
+        int remainingOunces = ounces % 16;
+        return ounces + " oz is equivalent to " + pounds + " lbs and " + remainingOunces + " oz.";
+    }
+
+    public static String toDecimalPounds(int ounces) {
+        double decimalPounds = ounces / 16.0;
+        return String.format("%d oz is equivalent to %.4f lbs.", ounces, decimalPounds);
+    }
 }
